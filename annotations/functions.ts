c970b85960134f08f29c1093bb42ca6e6830d03a
AmirAliEidivandi <amirah648887@gmail.com>
2022-10-29 00:20:44 +0330

    added object

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,10 +1,10 @@
 const add = (a: number, b: number): number => {
     return a + b;
-}
+};
 
 const subtract = (a: number, b: number) => {
     a - b;
-}
+};
 
 function divide(a: number, b: number): number {
     return a / b;
@@ -12,13 +12,13 @@ function divide(a: number, b: number): number {
 
 const multiply = function (a: number, b: number): number {
     return a * b;
-}
+};
 
 const logger = (message: string): void => {
     console.log(message);
     // return 'hello';
     return undefined;
-}
+};
 
 // const throwError = (message: string): never => {
 //     if (!message) {
@@ -27,3 +27,15 @@ const logger = (message: string): void => {
 
 //     return 'hello';
 // }
+
+const todaysWeather = {
+    date: new Date(),
+    weather: "sunny",
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void => {
+    console.log(forecast.date);
+    console.log(forecast.weather);
+};
+
+logWeather(todaysWeather);
